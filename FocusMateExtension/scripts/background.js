145aeb98612d466de876e9fa2084aa01f66341e4
Evgeniy Ashmarin <evgeniiashmarin@gmail.com>
2023-05-07 18:40:42 +0700

    bug fixes & refactoring

--- FocusMateExtension/scripts/background.js
+++ FocusMateExtension/scripts/background.js
@@ -3,14 +3,25 @@ const lastActiveTabKey = "lastActiveTab"; // {url: string, lastDateVal: number}
 
 chrome.windows.onFocusChanged.addListener(function (windowId) {
     if (windowId === chrome.windows.WINDOW_ID_NONE) {
-        processTabChange(false);
-    } else {
-        processTabChange(true);
+        processTabChange();
     }
 });
 
-function processTabChange(isWindowActive) {
-    chrome.tabs.query({"active": true}, function (tabs) {
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    processTabChange();
+});
+
+// chrome.windows.onRemoved.addListener(function (windowId) {
+//     chrome.storage.local.get([tabTimeObjectKey, lastActiveTabKey]).then( (storageData) => {
+//         let lastActiveTab = getLastActiveTab(storageData);
+//         let lastActiveTabUrl = lastActiveTab["url"];
+
+//     });
+// });
+
+function processTabChange(tabId) {
+    //chrome.tabs.get(tabId, function(tab) {
+    chrome.tabs.query({ "active": true }, function (tabs) {
         if (tabs.length === 0 || tabs[0] == null) {
             return;
         }
@@ -19,36 +30,71 @@ function processTabChange(isWindowActive) {
         let hostname = getHostname(currentTab);
 
         console.log(hostname)
-        console.log("isWindowActive: " + isWindowActive);
 
-        chrome.storage.local.get([tabTimeObjectKey, lastActiveTabKey], function (result) {
-            let tabTimeObjectString = result[tabTimeObjectKey];
-            let lastActiveTabString = result[lastActiveTabKey];
-
-            console.log(tabTimeObjectString);
-            console.log(lastActiveTabString);
-
-            let tabTime = {};
-            let lastActiveTab = {};
-            if (tabTimeObjectString != null) {
-                tabTime = JSON.parse(tabTimeObjectString);
-            }
-            if (lastActiveTabString != null) {
-                lastActiveTab = JSON.parse(lastActiveTabString);
-            }
-
-            let newLastTab = {};
-            if (isWindowActive) {
-                newLastTab[lastActiveTabKey] = JSON.stringify({"url": hostname, "lastDateVal": Date.now()});
-                chrome.storage.local.set(newLastTab, function () {
-                    let newTabTime = {};
-                    newTabTime[tabTimeObjectKey] = JSON.stringify(updateTabTime(tabTime, lastActiveTab));
-                    chrome.storage.local.set(newTabTime);
-                });
-            }
+        chrome.storage.local.get([tabTimeObjectKey, lastActiveTabKey]).then((storageData) => {
+            updateLocalStorageData(hostname, storageData);
         });
-
     });
+
+    function updateLocalStorageData(hostname, storageData) {
+        let tabTime = getTabTime(storageData);
+        let lastActiveTab = getLastActiveTab(storageData);
+
+        let newLastTab = {};
+        newLastTab[lastActiveTabKey] = JSON.stringify({ "url": hostname, "lastDateVal": Date.now() });
+        chrome.storage.local.set(newLastTab, function () {
+            let newTabTime = {};
+            newTabTime[tabTimeObjectKey] = JSON.stringify(updateTabTime(tabTime, lastActiveTab));
+            chrome.storage.local.set(newTabTime);
+        });
+    }
+
+    function updateTabTime(tabTime, lastActiveTab) {
+        let lastUrl = lastActiveTab["url"];
+        let passedSeconds = (Date.now() - lastActiveTab["lastDateVal"]) * 0.001;
+
+        let newTabTime = {};
+
+        if (tabTime.hasOwnProperty(lastUrl)) {
+            newTabTime["trackedSeconds"] = tabTime[lastUrl].trackedSeconds + passedSeconds;
+        } else {
+            newTabTime["trackedSeconds"] = passedSeconds;
+        }
+
+        newTabTime["lastDateVal"] = Date.now();
+
+        setTabTime(tabTime, lastUrl, newTabTime);
+
+        console.log(tabTime[lastUrl].trackedSeconds);
+
+        return tabTime;
+    }
+
+    function setTabTime(tabTime, url, value) {
+        tabTime[url] = { trackedSeconds: value["trackedSeconds"], lastDateVal: value["lastDateVal"] };
+    }
+
+    function getTabTime(storageData) {
+        let tabTimeObjectString = storageData[tabTimeObjectKey];
+        let tabTime = {};
+
+        if (tabTimeObjectString != null) {
+            tabTime = JSON.parse(tabTimeObjectString);
+        }
+
+        return tabTime;
+    }
+
+    function getLastActiveTab(storageData) {
+        let lastActiveTabString = storageData[lastActiveTabKey];
+        let lastActiveTab = {};
+
+        if (lastActiveTabString != null) {
+            lastActiveTab = JSON.parse(lastActiveTabString);
+        }
+
+        return lastActiveTab;
+    }
 
     function getHostname(tab) {
         let hostname;
@@ -58,28 +104,5 @@ function processTabChange(isWindowActive) {
             console.log(e);
         }
         return hostname;
-    }
-
-    function updateTabTime(tabTime, lastActiveTab) {
-        if (lastActiveTab.hasOwnProperty("url") && lastActiveTab.hasOwnProperty("lastDateVal")) {
-            let lastUrl = lastActiveTab["url"];
-            let passedSeconds = (Date.now() - lastActiveTab["lastDateVal"]) * 0.001;
-
-            if (tabTime.hasOwnProperty(lastUrl)) {
-                let lastUrlInfo = tabTime[lastUrl];
-
-                if (lastUrlInfo.hasOwnProperty("trackedSeconds")) {
-                    tabTime[lastUrl].trackedSeconds = tabTime[lastUrl].trackedSeconds + passedSeconds;
-                } else {
-                    tabTime[lastUrl].trackedSeconds = passedSeconds;
-                }
-
-                tabTime[lastUrl].lastDateVal = Date.now();
-            } else {
-                tabTime[lastUrl] = {url: lastUrl, trackedSeconds: passedSeconds, lastDateVal: Date.now()};
-            }
-        }
-
-        return tabTime;
     }
 }
--- FocusMateExtension/timer.js
+++ FocusMateExtension/timer.js
@@ -1,37 +1,47 @@
-try {
-    chrome.tabs.onUpdated.addListener(() => {
-        launchTimer()
-    });
+const tabTimeObjectKey = "tabTimeObject"; // {key: url, value: {trackedSeconds: number, lastDateVal: number}}
+const lastActiveTabKey = "lastActiveTab"; // {url: string, lastDateVal: number}
+let totalSeconds;
 
-    chrome.alarms.create("myAlarm", {delayInMinutes: 0.1, periodInMinutes: 0.1}, () => {
-        let counter = 0;
-        counter++;
-        console.log(counter);
-        setTime(counter);
-    });
+chrome.storage.local.get(tabTimeObjectKey, (result) => {
+    totalSeconds = Math.round(result[tabTimeObjectKey].trackedSeconds);
+    console.log(result[tabTimeObjectKey]);
+});
 
-    function align(value) {
-        if (value.length < 2) {
-            return "0" + value;
-        } else {
-            return value;
-        }
+setInterval(() => {
+    setTime(totalSeconds);
+    totalSeconds++;
+}, 1000);
+
+// chrome.tabs.onUpdated.addListener(() => {
+//     launchTimer()
+// });
+
+// chrome.alarms.create("myAlarm", {delayInMinutes: 0.1, periodInMinutes: 0.1}, () => {
+//     let counter = 0;
+//     counter++;
+//     console.log(counter);
+//     setTime(counter);
+// });
+
+function align(value) {
+    if (value.length < 2) {
+        return "0" + value;
+    } else {
+        return value;
     }
+}
 
-    function setTime(totalSeconds) {
-        let seconds = align(totalSeconds % 60);
-        let minutes = align(parseInt(totalSeconds / 60));
-        let hours = align(parseInt(totalSeconds / 3600));
-        document.getElementById("timer-text").innerHTML = hours + ":" + minutes + ":" + seconds;
-    }
+function setTime(totalSeconds) {
+    let seconds = align(totalSeconds % 60);
+    let minutes = align(parseInt(totalSeconds / 60));
+    let hours = align(parseInt(totalSeconds / 3600));
+    document.getElementById("timer-text").innerHTML = hours + ":" + minutes + ":" + seconds;
+}
 
-    function launchTimer() {
-        // setInterval(() => {
-        //     counter++;
-        //     console.log(counter);
-        //     setTime(counter);
-        // }, 1000);
-    }
-} catch (e) {
-    console.log(e);
+function launchTimer() {
+    // setInterval(() => {
+    //     counter++;
+    //     console.log(counter);
+    //     setTime(counter);
+    // }, 1000);
 }
