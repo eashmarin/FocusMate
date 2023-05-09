6819edfb4b0884b1c002420a87cf816fb3361371
Evgeny Ashmarin <evgeniiashmarin@gmail.com>
2023-05-09 22:41:09 +0700

    popup window features
    add table of limits and timer to popup window, include bootstrap
    prevent counting time while browser is unfocused

---
+++ FocusMateExtension/popup.js
@@ -0,0 +1,143 @@
+const tabTimeObjectKey = "tabTimeObject"; // {key: url, value: {trackedSeconds: number, lastDateVal: number}}
+const lastActiveTabKey = "lastActiveTab"; // {url: string, lastDateVal: number}
+let totalSeconds;
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("add-row-button").addEventListener("click", (e) => addRow());
+    document.getElementById("edit-table-button").addEventListener("click", (e) => madeTableEditable());
+    document.getElementById("save-table-button").addEventListener("click", (e) => saveTable());
+
+    chrome.storage.local.get(tabTimeObjectKey, (result) => {
+        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+            let activeTab = tabs[0];
+            let currentHostName = new URL(activeTab.url).hostname;
+            totalSeconds = Math.round(JSON.parse(result[tabTimeObjectKey])[currentHostName].trackedSeconds);
+        });
+    });
+});
+
+setInterval(() => {
+    setTime(totalSeconds);
+    totalSeconds++;
+}, 1000);
+
+
+function align(value) {
+    if (value.toString().length < 2) {
+        return "0" + value;
+    } else {
+        return value;
+    }
+}
+
+function setTime(totalSeconds) {
+    let seconds = align(totalSeconds % 60);
+    let minutes = align(parseInt(totalSeconds / 60) % 60);
+    let hours = align(parseInt(totalSeconds / 3600));
+    document.getElementById("timer-text").innerHTML = hours + ":" + minutes + ":" + seconds;
+}
+
+function openTab(event, tabName) {
+    let i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tab-links");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "flex";
+    event.currentTarget.className += " active";
+}
+
+function addRow() {
+    var table = document.getElementById("table-body");
+    var row = table.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var input1 = document.createElement("input");
+    var input2 = document.createElement("input");
+
+    input1.type = "text";
+    input2.type = "number";
+
+    cell1.appendChild(input1);
+    cell2.appendChild(input2);
+
+    input1.addEventListener("focusout", function () {
+        if (input1.value !== '') {
+            cell1.innerHTML = input1.value;
+        }
+    });
+    input2.addEventListener("focusout", function () {
+        if (input2.value !== '') {
+            cell2.innerHTML = input2.value;
+        }
+    });
+
+    document.getElementById("add-row-button").style.display = "none";
+    document.getElementById("edit-table-button").style.display = "none";
+    document.getElementById("save-table-button").style.display = "block";
+}
+
+function madeTableEditable() {
+    let table = document.getElementById("table-body");
+    const rows = table.getElementsByTagName('tr'); // получаем все строки таблицы
+
+    for (let i = 0; i < rows.length; i++) {
+        const cells = rows[i].getElementsByTagName('td'); // получаем все ячейки строки
+
+        for (let j = 0; j < cells.length; j++) {
+            const cell = cells[j];
+            const text = cell.innerHTML; // получаем содержимое ячейки
+
+            // создаем новый input и заменяем содержимое ячейки
+            const input = document.createElement('input');
+            input.type = 'text';
+            input.value = text;
+            cell.innerHTML = '';
+            cell.appendChild(input);
+        }
+    }
+
+    document.getElementById("add-row-button").style.display = "none";
+    document.getElementById("edit-table-button").style.display = "none";
+    document.getElementById("save-table-button").style.display = "block";
+}
+
+function saveTable() {
+    document.getElementById("add-row-button").style.display = "";
+    document.getElementById("edit-table-button").style.display = "";
+    document.getElementById("save-table-button").style.display = "none";
+
+    let table = document.getElementById("table-body");
+    let rows = table.getElementsByTagName('tr'); // получаем все строки таблицы
+
+    let hasEmptyRow = false;
+
+    for (var i = 0; i < rows.length; i++) {
+        var cells = rows[i].getElementsByTagName('td'); // получаем все ячейки строки
+
+        for (var j = 0; j < cells.length; j++) {
+            var cell = cells[j];
+            var input = cell.getElementsByTagName('input')[0]; // получаем первый input в ячейке
+
+            if (input) { // если есть input в ячейке
+                var value = input.value;
+                cell.removeChild(input);
+                cell.innerHTML = value;
+                if (input.value == "") {
+                    hasEmptyRow = true;
+                }
+            }
+        }
+    }
+
+    if (hasEmptyRow) {
+        table.deleteRow(-1);
+    }
+}
--- FocusMateExtension/scripts/background.js
+++ FocusMateExtension/scripts/background.js
@@ -1,6 +1,27 @@
 const tabTimeObjectKey = "tabTimeObject"; // {key: url, value: {trackedSeconds: number, lastDateVal: number}}
 const lastActiveTabKey = "lastActiveTab"; // {url: string, lastDateVal: number}
 
+let browserFocused = true;
+
+setInterval(checkBrowserFocus, 2000);
+
+function checkBrowserFocus() {
+    chrome.windows.getCurrent(function (browser) {
+        if (browserFocused === false && browser.focused === true) {
+            chrome.storage.local.get([tabTimeObjectKey, lastActiveTabKey]).then((storageData) => {
+                let lastActiveTab = getLastActiveTab(storageData);
+                let newLastTab = {};
+                console.log(lastActiveTab);
+                newLastTab[lastActiveTabKey] = JSON.stringify({"url": lastActiveTab.url, "lastDateVal": Date.now()});
+                chrome.storage.local.set(newLastTab);
+            });
+        }
+
+        browserFocused = browser.focused;
+        console.log(browserFocused);
+    });
+}
+
 chrome.windows.onFocusChanged.addListener(function (windowId) {
     if (windowId === chrome.windows.WINDOW_ID_NONE) {
         processTabChange();
@@ -11,98 +32,88 @@ chrome.tabs.onActivated.addListener(function (activeInfo) {
     processTabChange();
 });
 
-// chrome.windows.onRemoved.addListener(function (windowId) {
-//     chrome.storage.local.get([tabTimeObjectKey, lastActiveTabKey]).then( (storageData) => {
-//         let lastActiveTab = getLastActiveTab(storageData);
-//         let lastActiveTabUrl = lastActiveTab["url"];
-
-//     });
-// });
-
 function processTabChange(tabId) {
-    //chrome.tabs.get(tabId, function(tab) {
-    chrome.tabs.query({ "active": true }, function (tabs) {
-        if (tabs.length === 0 || tabs[0] == null) {
+    chrome.tabs.query({"active": true}, function (tabs) {
+        if (tabs.length === 0 || tabs[0] == null || !browserFocused) {
             return;
         }
 
         let currentTab = tabs[0];
         let hostname = getHostname(currentTab);
 
-        console.log(hostname)
-
         chrome.storage.local.get([tabTimeObjectKey, lastActiveTabKey]).then((storageData) => {
             updateLocalStorageData(hostname, storageData);
         });
     });
 
-    function updateLocalStorageData(hostname, storageData) {
-        let tabTime = getTabTime(storageData);
-        let lastActiveTab = getLastActiveTab(storageData);
+}
 
-        let newLastTab = {};
-        newLastTab[lastActiveTabKey] = JSON.stringify({ "url": hostname, "lastDateVal": Date.now() });
-        chrome.storage.local.set(newLastTab, function () {
-            let newTabTime = {};
-            newTabTime[tabTimeObjectKey] = JSON.stringify(updateTabTime(tabTime, lastActiveTab));
-            chrome.storage.local.set(newTabTime);
-        });
-    }
+function updateLocalStorageData(hostname, storageData) {
+    let tabTime = getTabTime(storageData);
+    let lastActiveTab = getLastActiveTab(storageData);
 
-    function updateTabTime(tabTime, lastActiveTab) {
-        let lastUrl = lastActiveTab["url"];
-        let passedSeconds = (Date.now() - lastActiveTab["lastDateVal"]) * 0.001;
-
+    let newLastTab = {};
+    newLastTab[lastActiveTabKey] = JSON.stringify({"url": hostname, "lastDateVal": Date.now()});
+    chrome.storage.local.set(newLastTab, function () {
         let newTabTime = {};
+        newTabTime[tabTimeObjectKey] = JSON.stringify(updateTabTime(tabTime, lastActiveTab));
+        chrome.storage.local.set(newTabTime);
+    });
+}
 
-        if (tabTime.hasOwnProperty(lastUrl)) {
-            newTabTime["trackedSeconds"] = tabTime[lastUrl].trackedSeconds + passedSeconds;
-        } else {
-            newTabTime["trackedSeconds"] = passedSeconds;
-        }
+function updateTabTime(tabTime, lastActiveTab) {
+    let lastUrl = lastActiveTab["url"];
+    let passedSeconds = (Date.now() - lastActiveTab["lastDateVal"]) * 0.001;
 
-        newTabTime["lastDateVal"] = Date.now();
+    let newTabTime = {};
 
-        setTabTime(tabTime, lastUrl, newTabTime);
-
-        console.log(tabTime[lastUrl].trackedSeconds);
-
-        return tabTime;
+    if (tabTime.hasOwnProperty(lastUrl)) {
+        newTabTime["trackedSeconds"] = tabTime[lastUrl].trackedSeconds + passedSeconds;
+    } else {
+        newTabTime["trackedSeconds"] = passedSeconds;
     }
 
-    function setTabTime(tabTime, url, value) {
-        tabTime[url] = { trackedSeconds: value["trackedSeconds"], lastDateVal: value["lastDateVal"] };
+    newTabTime["lastDateVal"] = Date.now();
+
+    setTabTime(tabTime, lastUrl, newTabTime);
+
+    console.log(tabTime[lastUrl].trackedSeconds);
+
+    return tabTime;
+}
+
+function setTabTime(tabTime, url, value) {
+    tabTime[url] = {trackedSeconds: value["trackedSeconds"], lastDateVal: value["lastDateVal"]};
+}
+
+function getTabTime(storageData) {
+    let tabTimeObjectString = storageData[tabTimeObjectKey];
+    let tabTime = {};
+
+    if (tabTimeObjectString != null) {
+        tabTime = JSON.parse(tabTimeObjectString);
     }
 
-    function getTabTime(storageData) {
-        let tabTimeObjectString = storageData[tabTimeObjectKey];
-        let tabTime = {};
+    return tabTime;
+}
 
-        if (tabTimeObjectString != null) {
-            tabTime = JSON.parse(tabTimeObjectString);
-        }
+function getLastActiveTab(storageData) {
+    let lastActiveTabString = storageData[lastActiveTabKey];
+    let lastActiveTab = {};
 
-        return tabTime;
+    if (lastActiveTabString != null) {
+        lastActiveTab = JSON.parse(lastActiveTabString);
     }
 
-    function getLastActiveTab(storageData) {
-        let lastActiveTabString = storageData[lastActiveTabKey];
-        let lastActiveTab = {};
+    return lastActiveTab;
+}
 
-        if (lastActiveTabString != null) {
-            lastActiveTab = JSON.parse(lastActiveTabString);
-        }
-
-        return lastActiveTab;
+function getHostname(tab) {
+    let hostname;
+    try {
+        hostname = new URL(tab.url).hostname;
+    } catch (e) {
+        console.log(e);
     }
-
-    function getHostname(tab) {
-        let hostname;
-        try {
-            hostname = new URL(tab.url).hostname;
-        } catch (e) {
-            console.log(e);
-        }
-        return hostname;
-    }
+    return hostname;
 }
